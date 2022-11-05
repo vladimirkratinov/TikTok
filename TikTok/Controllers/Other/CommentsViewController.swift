30ff68d8fad98694992ea3093148bad7e49cd22f
Vladimir Kratinov <vladimirkratinov@gmail.com>
2022-11-05 11:50:02 -0700

    add Custom Comment Cell

--- TikTok/Controllers/Other/CommentsViewController.swift
+++ TikTok/Controllers/Other/CommentsViewController.swift
@@ -20,8 +20,8 @@ class CommentsViewController: UIViewController {
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.register(
-            UITableViewCell.self,
-            forCellReuseIdentifier: "cell"
+            CommentTableViewCell.self,
+            forCellReuseIdentifier: CommentTableViewCell.identifier
         )
         return tableView
     }()
@@ -84,10 +84,22 @@ extension CommentsViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let comment = comments[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = comment.text
-        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CommentTableViewCell.identifier,
+            for: indexPath
+        ) as? CommentTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.configure(with: comment)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
  
